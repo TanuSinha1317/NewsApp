0f0959b685331f7bee96c42c46decca3f0389dfa
Tanu Rakesh Sinha <tanurakeshsinha@Tanus-MacBook-Air.local>
2026-04-27 01:21:08 +0530

    Converted project fully to Java and removed Kotlin dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,11 +34,12 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.core)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
